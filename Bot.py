2b104328c30939b696512cc2eeb7630945a9427f
MAX <maksimdemidovru@mail.ru>
2022-11-27 22:32:27 +0300

    Bot(V 1.1)

--- Bot.py
+++ Bot.py
@@ -1,47 +1,41 @@
-from telebot import types
 import telebot
+from telebot import types
 import random
-import os
 import time
-import requests
 
-count = 0
-dataclose = open("Play.txt",'w+',encoding="utf-8")
-dataclose.close 
-start_play = False
-start_calc = False
+count = 0                                          # счётчик для игры
+dataclose = open("Play.txt",'w+',encoding="utf-8") # очистка файла для игры во время запуска бота
+dataclose.close                                    #
+start_play = False                                 # переменная для начала игры
+start_calc = False                                 # переменная для вызова калькулятора
+
 bot = telebot.TeleBot("5838924178:AAH7l-u8BC4AQegn8Is-kzHW7-2u_GmdJ8s", parse_mode=None)
 
-markup = types.ReplyKeyboardMarkup(row_width=3)
-itembtn1 = types.KeyboardButton('Играть')
-itembtn2 = types.KeyboardButton('/calc')
+markup = types.ReplyKeyboardMarkup(row_width=3)  # Активные кнопки
+itembtn1 = types.KeyboardButton('Играть')        
+itembtn2 = types.KeyboardButton('Калькулятор')
 itembtn3 = types.KeyboardButton('/give_cat')
 markup.add(itembtn1, itembtn2, itembtn3)
-#tb.send_message(chat_id, 'Choose one letter:', reply_markup=markup)
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=['start']) # старт программы
 def send_welcome(message):
-    bot.reply_to(message, 'Привет, '+ message.from_user.first_name +'\nЧем займёмся?\nМогу посчитать тебе любую задачу,напиши /calc "твоя задача"\nМогу прислать тебе милого котика\n/give_cat\nМожем сыграть в игру, просто напиши Играть', reply_markup=markup)
+    bot.send_message(message.from_user.id, 'Привет, '+ message.from_user.first_name + ', открой меню', reply_markup=markup)
 
-@bot.message_handler(commands=['give_cat'])
+@bot.message_handler(commands=['give_cat']) # Призыв котика
 def Cat(message):
     cat = f'https://cataas.com/cat?t=${time.time()}'
     bot.send_photo(message.chat.id, cat)
 
-@bot.message_handler(content_types=["text"])
-def Calc(message):
-    print(f'{message.from_user.id} {message.from_user.first_name} {message.from_user.last_name}: {message.text}')
-    global start_calc
-    if 'Калькулятор' in message.text:
-        
-    bot.reply_to(message, eval(message.text[6:]))
 
-@bot.message_handler(content_types=['text'])
+
+@bot.message_handler(content_types=['text']) 
 def Play(message):
     print(f'{message.from_user.id} {message.from_user.first_name} {message.from_user.last_name}: {message.text}')
     global start_play
     global count
-    if start_play:
+    global start_calc
+    if start_play:                      # начало игры
         if message.text.isdigit():
             count= count + 1
             number_user = int(message.text)
@@ -50,9 +44,9 @@ def Play(message):
             number = int(number)
             data.close
             if number_user > number:
-                bot.send_message(message.from_user.id, 'Это число больше моего')
+                bot.send_message(message.from_user.id, 'Бери меньше')
             elif number_user < number:
-                bot.send_message(message.from_user.id, 'Это число меньше моего')
+                bot.send_message(message.from_user.id, 'Бери больше')
             elif number_user == number:
                 bot.send_message(message.from_user.id, f'Поздравляю, ты угадал!!\nКоличество попыток: {count}')
                 dataclose = open("Play.txt",'w+',encoding="utf-8")
@@ -60,7 +54,7 @@ def Play(message):
                 start_play = False
         else:
             bot.send_message(message.from_user.id, 'Введи число')
-    if 'Играть' in message.text:
+    if 'Играть' in message.text:                                        # Игра угадай число
             bot.send_message(message.from_user.id, 'Я загадал число от 1 до 1000, твоя задача угадать моё число, после каждой попытки я буду давать подсказку.\nВ конце игры я выведу количество твоих попыток.\nУдачи!') 
             start_play = True
             count = 0
@@ -68,9 +62,16 @@ def Play(message):
             data = open("Play.txt",'a+',encoding="utf-8")
             data.writelines(str(number)+'\n')
             data.close
-        
     
+    
+    if start_calc:                                              # Калькулятор
+        print(int(eval(message.text)))
+        bot.send_message(message.from_user.id, f"{message.text} = {int(eval(message.text))}")
+        start_calc = False
+    if 'Калькулятор' in message.text:
+        start_calc = True
+        bot.send_message(message.from_user.id, "Введи уравнение")
 
-
+    
 
 bot.infinity_polling()
